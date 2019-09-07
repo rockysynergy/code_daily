80b67726d54e841fca4bf7fe27a5eb740ada4617
Rocky Ou <rockysynergy@qq.com>
2019-09-07 09:23:41 +0800

    Create 87_location.php

---
+++ 87_location.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * A rule looks like this:
+
+A NE B
+
+This means this means point A is located northeast of point B.
+
+A SW C
+
+means that point A is southwest of C.
+
+Given a list of rules, check if the sum of the rules validate. For example:
+
+A N B
+B NE C
+C N A
+
+does not validate, since A cannot be both north and south of C.
+
+A NW B
+A N B
+
+is considered valid.
+ */
+
+ /**
+  * B['N'] = A
+    A['S'] = B
+    C['N'] = B, A -- since B['N'] = A
+    C['E'] = B
+    B['S'] = C
+    B['W'] = C
+    A['N'] = C, B, A
+    C['S'] = A, B -- A in both C['S'] and C['N'] thus invalid
+  */
